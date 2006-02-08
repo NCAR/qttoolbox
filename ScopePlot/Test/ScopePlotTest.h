1256e269fc1566befdb48d49d132d85eed003b41
martinc <martinc@da89c675-a4d7-0310-8a15-9e7546503515>
2006-02-08 19:05:54 +0000

    Added plot type selection to the ScopePlot test. Axes type changing currently commented out, since it causes run time crashes. There must be something wrong with the way that the scale engine is used.

--- ScopePlot/Test/ScopePlotTest.h
+++ ScopePlot/Test/ScopePlotTest.h
@@ -2,13 +2,20 @@
 #define SCOPEPLOTTEST_H
 
 #include "ScopePlotTestBase.h"
+#include "ScopePoke.h"
 
 class ScopePlotTest: public ScopePlotTestBase {
 
 public:
    ScopePlotTest();
    ~ScopePlotTest();
+   void setScopePoke(ScopePoke* scopePoke);
 
+public slots:
+    virtual void plotTypeSlot(bool);
+
+protected:
+   ScopePoke* _scopePoke;
 };
 
 #endif

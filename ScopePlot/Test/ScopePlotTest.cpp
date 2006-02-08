3f80d9c487978d328142aa7cdf6d01b4a6ba8fe9
martinc <martinc@da89c675-a4d7-0310-8a15-9e7546503515>
2006-02-08 19:04:56 +0000

    Added plot type selection to the ScopePlot test. Axes type changing currently commented out, since it causes run time crashes. There must be something wrong with the way that the scale engine is used.

--- ScopePlot/Test/ScopePlotTest.cpp
+++ ScopePlot/Test/ScopePlotTest.cpp
@@ -1,8 +1,40 @@
-#include "ScopePlotTest.h"
 
-ScopePlotTest::ScopePlotTest() {
+#include "ScopePlotTest.h"
+#include "ScopePlot.h"
+#include <qbuttongroup.h>
+
+ScopePlotTest::ScopePlotTest():
+_scopePoke(0)
+{
 }
 
 ScopePlotTest::~ScopePlotTest() {
+}
+
+void
+ScopePlotTest::setScopePoke(ScopePoke* scopePoke) {
+   _scopePoke = scopePoke;
+}
+
+void 
+ScopePlotTest::plotTypeSlot(bool b)
+{
+
+   switch (buttonGroup->selectedId()) {
+      case 0:
+         if (_scopePoke)
+            _scopePoke->setPlotType(ScopePlot::TIMESERIES);
+         break;
+      case 1:
+         if (_scopePoke)
+            _scopePoke->setPlotType(ScopePlot::IVSQ);
+         break;
+      case 2:
+         if (_scopePoke)
+            _scopePoke->setPlotType(ScopePlot::SPECTRUM);
+         break;
+      default: {}
+   }
+
 }
 

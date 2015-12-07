7449422d7c2119c57779b1e8abdf715c151a1e0e
Gordon Maclean <maclean@users.noreply.github.com>
2015-12-06 19:07:46 -0700

    Add script to mirror commits to subversion

---
+++ scripts/mirror_git_to_svn/push_svn.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+repo=qttoolbox
+
+svnurl=http://svn.eol.ucar.edu/svn/$repo
+
+if ! [ $JENKINS_HOME ]; then
+
+    # for testing outside of jenkins
+    jenkinsrepo=/tmp/${repo}_jenkins
+    giturl=https://github.com/ncareol/$repo.git
+
+    if [ -d $jenkinsrepo ]; then
+        cd $jenkinsrepo
+        git pull --rebase origin master
+    else
+        git clone $giturl $jenkinsrepo
+        cd $jenkinsrepo
+    fi
+fi
+
+if ! [ -f .git/config ] || ! grep -F -q svn-remote .git/config; then
+    cat << EOD >> .git/config
+[svn-remote "svn"]
+    url = $svnurl
+    fetch = :refs/remotes/git-svn
+EOD
+fi
+
+# fetch objects from svn-remote named "svn"
+# takes a long time the first time it is run from a large repo
+git svn fetch svn
+
+# create svn branch tracking git-svn remote
+# must be done after above git svn fetch
+git show-ref --verify --quiet refs/heads/svn || git branch svn git-svn
+
+# rebase master so it becomes series of commits after HEAD of svn
+# takes a long time the first time it is run from a large repo
+git rebase svn master
+
+git checkout svn
+
+# fast-forward merge the new commits on master back to svn
+git merge --ff-only master
+
+# commit new commits to subversion
+git svn dcommit
+
+# set master back to origin/master
+git rebase origin/master master
+
+# svn log $svnurl

a69ee771f322d1a8e0008fa7f183a1a5a80ed734
Gordon Maclean <maclean@users.noreply.github.com>
2015-12-08 15:00:31 -0700

    Tweaks to push_svn.sh
    Use "git config" to setup svn-remote configuration, rather
    than direct edits of .git/config.

--- scripts/mirror_git_to_svn/push_svn.sh
+++ scripts/mirror_git_to_svn/push_svn.sh
@@ -8,6 +8,7 @@ svnurl=http://svn.eol.ucar.edu/svn/$repo
 if ! [ $JENKINS_HOME ]; then
 
     JENKINS_HOME=/tmp/${repo}_jenkins
+    giturl=https://github.com/ncareol/${repo}.git
 
     [ -d $JENKINS_HOME ] || mkdir $JENKINS_HOME
     cd $JENKINS_HOME
@@ -25,40 +26,37 @@ if ! [ $JENKINS_HOME ]; then
 
 fi
 
-if ! [ -f .git/config ] || ! grep -F -q svn-remote .git/config; then
-    cat << EOD >> .git/config
-[svn-remote "svn"]
-    url = $svnurl
-    fetch = :refs/remotes/git-svn
-EOD
+# setup svn-remote configuration
+if ! git config --get-regexp svn-remote.svn > /dev/null; then
+    git config svn-remote.svn.url $svnurl
+    git config svn-remote.svn.fetch ":refs/remotes/git-svn"
 fi
 
-# fetch objects from svn-remote named "svn"
+# fetch from svn-remote named "svn"
 # takes a long time the first time it is run from a large repo
-git svn fetch svn
+git svn fetch svn 
 
-# create tmp-master branch pointing at latest commit
+# create svn branch if it doesn't exist, tracking git-svn remote
+# must be done after above git svn fetch
+git show-ref --verify --quiet refs/heads/svn || git branch svn git-svn
+
+# create new tmp-master branch pointing at latest commit
 git show-ref --verify --quiet refs/heads/tmp-master && git branch -D tmp-master
 git branch tmp-master $GIT_COMMIT
 git checkout tmp-master
 
-# create svn branch, tracking git-svn remote
-# must be done after above git svn fetch
-git show-ref --verify --quiet refs/heads/svn || git branch svn git-svn
-
-# rebase tmp-master so it becomes series of commits after HEAD of svn
-# takes a long time the first time it is run from a large repo
+# rebase tmp-master so it becomes a series of commits after HEAD of svn
+# takes a long time the first time it is run on a large repo
 git rebase svn
 
+# fast-forward merge the new commits to svn
 git checkout svn
-
-# fast-forward merge the new commits on tmp-master to svn
 git merge --ff-only tmp-master
 
-# commit new commits to subversion
+# push new commits to subversion
 git svn dcommit
 
-# reset working tree back to original state
+# reset working tree
 git checkout -f $GIT_COMMIT
 
 # delete temporary branch
